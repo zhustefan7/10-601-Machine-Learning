da3bafdf1bf6c83b0ea4693b74a58bcbafa9fe16
Stefan Zhu <zhuzheyao7@gmail.com>
2019-09-06 19:21:06 -0400

    finished parse file

--- F19_10601_HW2/decisionTree.py
+++ F19_10601_HW2/decisionTree.py
@@ -1,5 +1,6 @@
-from decisionTree import parse_label
-
+from decisionTree import*
+import csv
+import math
 
 
 def parse_file(file_path):
@@ -7,7 +8,14 @@ def parse_file(file_path):
     with open(file_path) as file:
         reader = csv.reader(file, delimiter='\t')
         headers = next(reader)
+        for i in range(len(headers)):
+            map[i]=[]
         for row in reader:
+            for i in range(len(row)):
+                map[i].append(row[i])
+    return map
+
+
             
 
 
@@ -19,6 +27,23 @@ def calc_marginal_entropy(file_path):
     entropy = -(minority/total*math.log(minority/total,2)+majority/total*math.log(majority/total,2))
     return entropy
 
-def calc_conditional_entropy
+def calc_conditional_entropy(map,attribute):
+    data_type = {}
+    for data in map[attribute]:
+        if data not in data_type:
+            data_type[data] = 1 
+        else: 
+            data_type[data] +=1
+            
+    
 
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    parse_file('handout/politicians_train.tsv'  )
 
