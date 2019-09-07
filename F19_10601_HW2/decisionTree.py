686cd70683cc6287a8e62dc27f02c1cda3efb6e7
Stefen7 <stefen7@Stefen7s-MacBook-Pro.local>
2019-09-07 18:24:17 -0400

    mutual information calculation working

--- F19_10601_HW2/decisionTree.py
+++ F19_10601_HW2/decisionTree.py
@@ -1,4 +1,4 @@
-from decisionTree import*
+from inspect import*
 import csv
 import math
 import numpy as np
@@ -41,20 +41,34 @@ def stat_analsysis(map):
 
     
 
-def calc_marginal_entropy(file_path):
+# def calc_marginal_entropy(file_path):
+#     """
+#     calculates the marginal entropy of the labels of each data sheet
+#     """
+#     map = parse_label(file_path)
+#     majority = max(map.values())
+#     minority = min(map.values())
+#     total = float(majority + minority)
+#     entropy = -(minority/total*math.log(minority/total,2)+majority/total*math.log(majority/total,2))
+#     return entropy
+
+def calc_marginal_entropy(data_stat):
     """
     calculates the marginal entropy of the labels of each data sheet
     """
-    map = parse_label(input_file)
-    majority = max(map.values())
-    minority = min(map.values())
+    label_col = len(data_stat)-1
+    label_info = data_stat[label_col]
+    majority = max(label_info.values())
+    minority = min(label_info.values())
     total = float(majority + minority)
     entropy = -(minority/total*math.log(minority/total,2)+majority/total*math.log(majority/total,2))
     return entropy
 
+
+
 def calc_conditional_entropy(map,data_stat,attribute):
     """
-    calculates the conditional 
+    calculates the conditional entropy of a specific attribute
     """
     #acquire the data info of the attribute stored in data_stat
     data_info = data_stat[attribute]
@@ -64,40 +78,38 @@ def calc_conditional_entropy(map,data_stat,attribute):
     #acquire the data 
     data = map[attribute]
     labels = map[label_col]
-
-    specific_entropy = {}
+    conditional_entropy =0
     for data_type in data_info:
+        specific_entropy = 0
         for label_type in label_info:  
-            #attribute data where all data entries are equal to a speicifc value
-            data_with_spec_val_idx = np.where(data==data_type)
+            #attribute data indices where all data entries are equal to a speicifc value
+            data_with_spec_val_idx = np.where(data==data_type)[0]
+            #label indices where all labels are of same value
             spec_label_idx = np.where(labels==label_type)
-            intersect = np.intersect1d(data_with_spec_val_idx,spec_label_idx)
-            print('#############')
-            print('data type', data_type)
-            print('label_type',label_type)
-            print(data_with_spec_val_idx)
-            print(spec_label_idx)
-            print(intersect)
-
-
-            # print(np.union1d(data ==data_type , labels_data =label_type))
-            # data_with_spec_val = data[data==data_type]
-            # #attribute data with specific value which satisfies speicifc label type
-            # conditional_data = data_with_spec_val[labels==label_type]
-            # temp_prob = conditional_data/data_with_spec_val
-            # print(temp_prob)
-            # # temp = -(len(map[attribute]==data_type and map[label_col]==label_type)/len(map[attribute]==data_type))
-            # # print(temp)
-        
-
+            #the intersection of the two indices above
+            intersect_idx = np.intersect1d(data_with_spec_val_idx,spec_label_idx)
+            #conditional probability of label being of specific value given speicific data value
+            temp_prob = len(intersect_idx)/float(len(data_with_spec_val_idx))
+            specific_entropy += temp_prob*math.log(temp_prob,2)
+        specific_entropy = -specific_entropy
+        prob = len(data_with_spec_val_idx)/float(len(data))
+        conditional_entropy += prob * specific_entropy
     
-    
-        
+    return conditional_entropy
             
-    
+def calc_mutual_info(map,data_stat,attribute):
+    conditional_entropy = calc_conditional_entropy(map,data_stat,attribute)
+    marginal_entropy = calc_marginal_entropy(data_stat)
+    mutual_info = marginal_entropy - conditional_entropy
+    return mutual_info
 
+    
+        
 if __name__ == "__main__":
     map = parse_file('handout/politicians_train.tsv')
     data_stat=stat_analsysis(map)
-    calc_conditional_entropy(map,data_stat,0)
+    mutual_info = calc_mutual_info(map, data_stat, 3)
+    # marginal_entropy=calc_marginal_entropy2(data_stat)
+    # calc_conditional_entropy(map,data_stat,0)
+    print(mutual_info)
 
